90d2b24fb1fa6fc88ea3ec0f173d6afc6d582584
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2020-02-14 07:33:33 +0100

    autocommit

--- fbird.py
+++ fbird.py
@@ -2,10 +2,6 @@ import pgzrun
 from random import randint
 
 
-def init_walls():
-    return [[200, 200, False], [500, 300, False]]
-
-
 def init_bird():
     return 200
 
@@ -14,9 +10,17 @@ def init_speed():
     return -1
 
 
+def init_walls():
+    return [[200, 200, False], [500, 300, False]]
+
+
+def init_score():
+    return 0
+
+
 def draw_bird():
-    b = Rect((100, bird), (GAP, GAP))
-    screen.draw.filled_rect(b, 'YELLOW')
+    f = Rect((100, bird), (GAP, GAP))
+    screen.draw.filled_rect(f, 'YELLOW')
 
 
 def draw_walls():
@@ -27,6 +31,31 @@ def draw_walls():
         b = Rect((x, y + 7 * GAP), (3 * GAP, HEIGHT - y + 7 * GAP))
         screen.draw.filled_rect(t, 'GREEN')
         screen.draw.filled_rect(b, 'GREEN')
+
+
+def draw():
+    screen.fill('BLUE')
+    draw_bird()
+    draw_walls()
+    screen.draw.text(str(score), (20, 100), color='RED', fontsize=60)
+
+
+def update():
+    global bird, speed, walls, score
+    calc_speed()
+    check_keys()
+    move_walls()
+    dead = check_collision()
+    if is_out() or dead:
+        bird = init_bird()
+        speed = init_speed()
+        score = init_score()
+
+
+def is_out():
+    if bird > HEIGHT or bird < 0:
+        return True
+    return False
 
 
 def check_collision():
@@ -46,31 +75,6 @@ def check_collision():
         if f.colliderect(t) or f.colliderect(b):
             dead = True
     return dead
-
-
-def draw():
-    screen.fill('BLUE')
-    draw_bird()
-    draw_walls()
-    screen.draw.text(str(score), (20, 100), color='RED', fontsize=60)
-
-
-def update():
-    global bird, speed, walls, score
-    calc_speed()
-    check_keys()
-    move_walls()
-    dead = check_collision()
-    if is_dead() or dead:
-        bird = init_bird()
-        speed = init_speed()
-        score = init_score()
-
-
-def is_dead():
-    if bird > HEIGHT or bird < 0:
-        return True
-    return False
 
 
 def calc_speed():
@@ -98,10 +102,6 @@ def move_walls():
             wall[1] = 100 * randint(1, 3)
             wall[2] = False
             print(walls)
-
-
-def init_score():
-    return 0
 
 
 # globals
